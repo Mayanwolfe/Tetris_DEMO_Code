e400571db9907933c07b6371276981ac85d57d45
Claire Bourdon <cebourdon625@gmail.com>
2022-12-04 18:56:53 -0500

    updating main.js

--- main.js
+++ main.js
@@ -1,7 +1,41 @@
 const canvas = document.getElementById('board')
 const ctx = canvas.getContext('2d')
+const canvasNext = document.getElementById('next')
+const ctxNext = canvas.getContext('2d')
 
-let board = new Board();
+let accountValues = {
+    score: 0,
+    level: 0,
+    lines: 0
+}
+
+function updateAccount(key,value) {
+    let element = getElementById(key)
+    if (element) {
+        element.textContent = value
+    }
+}
+
+let account = new Proxy(accountValues, {
+    set: (target, key, value) => {
+        target[key] = value
+        updateAccount(key,value)
+        return true
+    }
+})
+
+let requestId;
+
+
+let board = new Board(ctx, ctxNext)
+addEventListener()
+initNext()
+
+function initNext() {
+    ctxNext.canvas.width = 4 * BLOCK_SIZE
+    ctxNext.canvas.height = 4 * BLOCK_SIZE
+    ctxNext.scale(BLOCK_SIZE,BLOCK_SIZE)
+}
 
 function play() {
     board.reset()
@@ -19,8 +53,14 @@ const moves = {
     [KEY.UP]: p => board.rotate(p)
   };
 
-document.addEventListener('keydown', event => {
-    if(moves[event.keyCode]) {
+function addEventListener() {
+    document.addEventListener('keydown', event => {
+        if(event.keyCode === KEY.P) {
+           pause()
+        }
+        if(event.keyCode === KEY.ESC) {
+            gameOver()
+        } else if(moves[event.keyCode]) {
         event.preventDefault()
 
         let p = moves[event.keyCode](board.piece);
@@ -28,16 +68,17 @@ document.addEventListener('keydown', event => {
         if (event.keyCode === KEY.SPACE) {
             // Hard drop
             while (board.valid(p)) {
+                account.score += POINTS.HARD_DROP
               board.piece.move(p);   
               p = moves[KEY.DOWN](board.piece);
             }
           }
           else if (board.valid(p)) {
             board.piece.move(p);
+            if (event.keyCode === KEY.DOWN) {
+                account.score += POINTS.SOFT_DROP
+            }
           }
-        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-
-        board.piece.draw()
-        
     }
 })
+}
