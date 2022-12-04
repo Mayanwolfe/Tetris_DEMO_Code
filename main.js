55399ccd39689672058e52bde240510795a368f5
Claire Bourdon <cebourdon625@gmail.com>
2022-12-04 16:41:40 -0500

    added piece coordinate changes

--- main.js
+++ main.js
@@ -15,3 +15,25 @@ function play() {
 
     board.piece = piece
 }
+
+const moves = {
+    [KEY.LEFT]:  p => ({ ...p, x: p.x - 1 }),
+    [KEY.RIGHT]: p => ({ ...p, x: p.x + 1 }),
+    [KEY.DOWN]:    p => ({ ...p, y: p.y + 1 })
+  };
+
+document.addEventListener('keydown', event => {
+    if(moves[event.keyCode]) {
+        event.preventDefault()
+
+        let p = moves[event.keyCode](board.piece);
+
+        //if (board.valid(p)) {
+            board.piece.move(p)
+
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+
+        board.piece.draw()
+        //}
+    }
+})
--- piece.js
+++ piece.js
@@ -22,4 +22,9 @@ class Piece {
             })
         })
     }
+
+    move(p) {
+        this.x = p.x;
+        this.y = p.y;
+    }
 }
