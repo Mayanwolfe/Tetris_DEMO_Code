129bf692126281a844b1af4f2e453a1358becead
Claire Bourdon <cebourdon625@gmail.com>
2022-12-04 18:15:26 -0500

    added board methods

--- board.js
+++ board.js
@@ -1,9 +1,85 @@
 class Board {
-    reset() {
-        this.grid = this.getEmptyBoard()
+    ctx;
+    ctxNext;
+    grid;
+    piece;
+    next;
+    requestId;
+    time;
+
+    constructor(ctx, ctxNext) {
+        this.ctx = ctx
+        this.ctxNext = ctxNext
+        this.init()
     }
 
-    getEmptyBoard() {
+    init() {
+        ctx.canvas.width = COLS * BLOCK_SIZE;
+        ctx.canvas.height = ROWS * BLOCK_SIZE;
+
+        ctx.scale(BLOCK_SIZE,BLOCK_SIZE)
+    }
+
+    reset() {
+        this.grid = this.getEmptyGrid()
+        this.piece = new Piece(this.ctx)
+        this.piece.setStartingPosition()
+        this.getNewPiece()
+    }
+
+    getNewPiece() {
+        this.next = new Piece(this.ctxNext)
+        this.ctxNext.clearRect(0,0,this.ctxNext.canvas.with,this.ctxNext.canvas.height);
+        this.next.draw()
+    }
+
+    draw() {
+        this.piece.draw()
+        this.drawBoard()
+    }
+
+    drop() {
+        let p = moves[KEY.DOWN](this.piece)
+        if (this.valid(p)) {
+            this.piece.move(p)
+        } else {
+            this.freeze()
+            this.clearLines()
+            if(this.piece.y === 0) {
+                // Game over
+                return false
+            }
+            this.piece = this.next
+            this.piece.ctx = this.ctx
+            this.piece.setStartingPosition()
+            this.getNewPiece()
+        }
+        return true
+    }
+
+    clearLines () {
+        let lines = 0
+        this.grid.forEach((row,y) => {
+            if(row.evey(value => value >0)) {
+                lines++
+                this.grid.splice(y,1)
+                this.grid.unshift(Array(COLS).fill(0))
+            }
+        })
+
+        if (lines>0) {
+            account.score += this.getLinesClearedPoints(lines)
+            account.lines += lines
+
+            if(account.lines >= LINES_PER_LEVEL) {
+                account.level++
+                account.lines -= LINES_PER_LEVEL
+                time.level = LEVEL[account.level]
+            }
+        }
+    }
+
+    getEmptyGrid() {
         return Array.from(
             {length: ROWS}, () => Array(COLS).fill(0)
         )
@@ -15,7 +91,8 @@ class Board {
                 let x = p.x + dx
                 let y = p.y + dy
                 return (
-                    this.insideWalls(x) && this.aboveFloor(y)
+                    value === 0 ||
+                    (this.insideWalls(x) && this.aboveFloor(y) && this.notOccupied(x,y))
                 )
             })
         })
@@ -29,6 +106,10 @@ class Board {
         return y <= ROWS
     }
 
+    notOccupied(x,y) {
+        return this.grid[y] && this.grid[y][x] ===0
+    }
+
     rotate(p) {
         let clone = JSON.parse(JSON.stringify(p));
         for (let y = 0; y < p.shape.length; ++y) {
@@ -40,6 +121,8 @@ class Board {
           
           // Reverse the order of the columns.
           p.shape.forEach(row => row.reverse());
-          return clone;
+          return p;
     }
+
+
 }
