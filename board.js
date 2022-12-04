3eeebcb8d7f2dce400a3ae36b128781930420270
Claire Bourdon <cebourdon625@gmail.com>
2022-12-04 17:12:19 -0500

    added rotation

--- board.js
+++ board.js
@@ -28,4 +28,18 @@ class Board {
     aboveFloor(y) {
         return y <= ROWS
     }
+
+    rotate(p) {
+        let clone = JSON.parse(JSON.stringify(p));
+        for (let y = 0; y < p.shape.length; ++y) {
+            for (let x = 0; x < y; ++x) {
+              [p.shape[x][y], p.shape[y][x]] = 
+              [p.shape[y][x], p.shape[x][y]];
+            }
+          }
+          
+          // Reverse the order of the columns.
+          p.shape.forEach(row => row.reverse());
+          return clone;
+    }
 }
--- main.js
+++ main.js
@@ -19,7 +19,9 @@ function play() {
 const moves = {
     [KEY.LEFT]:  p => ({ ...p, x: p.x - 1 }),
     [KEY.RIGHT]: p => ({ ...p, x: p.x + 1 }),
-    [KEY.DOWN]:    p => ({ ...p, y: p.y + 1 })
+    [KEY.DOWN]:    p => ({ ...p, y: p.y + 1 }),
+    [KEY.SPACE]: p => ({ ...p, y: p.y + 1 }),
+    [KEY.UP]: p => board.rotate(p)
   };
 
 document.addEventListener('keydown', event => {
@@ -28,12 +30,19 @@ document.addEventListener('keydown', event => {
 
         let p = moves[event.keyCode](board.piece);
 
-        if (board.valid(p)) {
-            board.piece.move(p)
-
+        if (event.keyCode === KEY.SPACE) {
+            // Hard drop
+            while (board.valid(p)) {
+              board.piece.move(p);   
+              p = moves[KEY.DOWN](board.piece);
+            }
+          }
+          else if (board.valid(p)) {
+            board.piece.move(p);
+          }
         ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
 
         board.piece.draw()
-        }
+        
     }
 })
